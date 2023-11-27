9e97459bbe257b4b5e1bbc905eb0de4a298b5020
peitianyu <1216582461@qq.com>
2023-11-28 07:58:54 +0800

    add: 添加parse_code

--- src/stc/parse_code.h
+++ src/stc/parse_code.h
@@ -4,17 +4,88 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <cctype>
 
+enum Mnemonic
+{
+    Identifier,
+    OP_ADD,
+    OP_ADDC,
+    OP_INC,
+    OP_SUBB,
+    OP_DEC,
+    OP_MUL,
+    OP_DIV,
+    OP_DA,
+    OP_ANL,
+    OP_ORL,
+    OP_XRL,
+    OP_CLR,
+    OP_CPL,
+    OP_RL,
+    OP_RLC,
+    OP_RR,
+    OP_RRC,
+    OP_SWAP,
+    OP_MOV,
+    OP_MOVC,
+    OP_MOVX,
+    OP_PUSH,
+    OP_POP,
+    OP_XCH,
+    OP_XCHD,
+    OP_SETB,
+    OP_ACALL,
+    OP_LCALL,
+    OP_RET,
+    OP_RETI,
+    OP_AJMP,
+    OP_LJMP,
+    OP_SJMP,
+    OP_JZ,
+    OP_JNZ,
+    OP_JC,
+    OP_JNC,
+    OP_JB,
+    OP_JNB,
+    OP_JBC,
+    OP_CJNE,
+    OP_DJNZ,
+    OP_NOP,
+    REG_A,
+    REG_R0,
+    REG_R1,
+    REG_R2,
+    REG_R3,
+    REG_R4,
+    REG_R5,
+    REG_R6,
+    REG_R7,    
+    REG_DPTR,
+    REG_AB,
+    REG_PC,
+    REG_C,
+    TOKEN_SEP_COMMA,
+    TOKEN_OP_LEN,
+    TOKEN_SEP_DOT,
+    TOKEN_SEP_PLUS,
+    Number,
+    TOKEN_SEP_COLON,
+    Directive_ORG,
+    Directive_END,
+    Directive_DB,
+    Directive_EQU,
+    TOKEN_SEP_ARE,
+    TOKEN_SEP_SLASH,
+    OP_JMP,
+    TOKEN_SEP_DOLLAR,
+    TOKEN_Symbol = Identifier,
+};
 struct Code
 {
     std::size_t addr;
-    std::string mnemonic;
+    Mnemonic mnemonic;
     std::string operands;
-};
-
-enum Token
-{
-    
 };
 
 class ParseCode
@@ -22,10 +93,27 @@ class ParseCode
 public:
     ParseCode(const std::string& code)
     {
+        static std::size_t addr = 0;
+        if(is_table(code)) m_codes.push_back({addr, TOKEN_Symbol, get_colon(code)});
 
+        addr++;
     }
 private:
-    stc::vector<Code> m_codes;
+    bool is_table(const std::string& code)
+    {
+        std::size_t pos = code.find_first_not_of(':');
+        return (pos != std::string::npos);
+    }
+
+    std::string get_colon(const std::string& code) 
+    {
+        std::size_t pos = code.find(':');
+        if (pos != std::string::npos) return code.substr(0, pos); 
+           
+        return code;
+    }
+private:
+    std::vector<Code> m_codes;
 };
 
 
--- src/test/read_file_test.cc
+++ src/test/read_file_test.cc
@@ -1,6 +1,7 @@
 #include "core/tt_test.h"
 #include "core/log.h"
 #include "stc/parse_file.h"
+#include "stc/parse_code.h"
 #include <iostream>
 
 JUST_RUN_TEST(paser, test)
@@ -8,7 +9,7 @@ TEST(paser, test)
 {
     LOG_TEST("------------------------------paser--------------------------");
 
-    ParseFile parse_file("/root/work_space/stc/tt_stc/data/0_led.S");
+    ParseFile parse_file("/root/work_space/stc/tt8051/data/0_led.S");
 
     while(true)
     {
